2fd4034cdbe5ffc1c4fd6d0d08a6e8cf417db078
Chris Chen <chrischen79@gmail.com>
2020-05-14 18:02:53 +1000

    chore: add code linter and related tooling

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'no-console': 'warn',
+  },
+}
